553b1a85b8a4f1193d72bcb22cdf2d437940cdac
vaish567 <vaishnav.parte@gmail.com>
2024-06-15 17:01:22 +0530

    Remove Twilio secret and use environment variable

--- backend/src/services/twilioService.js
+++ backend/src/services/twilioService.js
@@ -1,6 +1,6 @@
 const twilio = require('twilio');
 
-const client = twilio('ACee47205c8447f9d55a2603227334c314', '7022ec0a4c1f567db39fffc4e571d716');
+const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_SECRET_KEY);
 
 exports.sendSMS = (to, body) => {
   return client.messages.create({
